5e30509cf35b3937a650103ab0540946c69a9b03
alphabetav <alphavo@gmail.com>
2022-08-23 12:51:31 -0700

    modified the html file to include a form + reset button. The form dynamically changes the number of squares from 1 -> 100, whereas the reset button resets all the background colours of the square to white.
    I also updated the javascript to account for these dynamic functions

--- etch.js
+++ etch.js
@@ -4,6 +4,11 @@ var boxNumbers = 16
 
 var height = 100/boxNumbers
 var width = 100/boxNumbers
+var boxes = document.querySelectorAll('.box')
+
+const resetter = document.querySelector('#reset')
+const resizer = document.querySelector('#resizer')
+
 
 function lineMaker() {
     let newDiv = document.createElement('div')
@@ -25,16 +30,55 @@ function blackBox(e) {
     this.style.backgroundColor = 'black'
 }
 
-for (let x = 0; x < boxNumbers; x++) {
-    lineMaker()
-    for (let y = 0; y < boxNumbers; y++) {
-        boxMaker(x)
+function trueBoxerMaker() {
+    for (let x = 0; x < boxNumbers; x++) {
+        lineMaker()
+        for (let y = 0; y < boxNumbers; y++) {
+            boxMaker(x)
+        }
     }
 }
 
-var boxes = document.querySelectorAll('.box')
+function boxRemover() {
+    let child = container.firstElementChild
+    while(child) {
+        container.removeChild(child)
+        child = container.firstElementChild
+    }
+}
 
-boxes.forEach(box => box.addEventListener('mouseover', blackBox))
+function numberSetter() {
+    boxNumbers = document.querySelector('input').value
+    height = 100/boxNumbers
+    width = 100/boxNumbers
+}
+
+function boxRemover() {
+    let child = container.firstElementChild
+    while(child) {
+        container.removeChild(child)
+        child = container.firstElementChild
+    }
+}
+
+function totalBoxMaker() {
+    boxRemover()
+    numberSetter()
+    trueBoxerMaker()
+    boxes = document.querySelectorAll('.box')
+    boxes.forEach(box => box.addEventListener('mouseover', blackBox))
+
+}
+
+resetter.addEventListener('click', () => {
+    boxes.forEach(box => box.style.backgroundColor = 'white')
+})
+
+resizer.addEventListener('keyup', () => {
+    if (document.querySelector('input').value >= 1 && document.querySelector('input').value <= 100) {
+        totalBoxMaker()
+    }
+})
 
 // create button that submits how many boxes there will be
 // deletes any existing boxes with the selection
@@ -44,3 +88,65 @@ boxes.forEach(box => box.addEventListener('mouseover', blackBox))
 // create button that changes the background color of boxes back to white, but does not reset amount of boxes
 // add eventlister button submission or click
 // boxes.forEach(box => box.style.backgroundColor = 'white') <<< could be the code for this simple function which is called
+
+
+// use this when destroying nodes
+//function removeAllChildNodes(parent) {
+//    while (parent.firstChild) {
+//        parent.removeChild(parent.firstChild);
+//    }
+//}
+//const container = document.querySelector('#container');
+//removeAllChildNodes(container);
+
+//box creator initial
+//for (let x = 0; x < boxNumbers; x++) {
+//    lineMaker()
+//    for (let y = 0; y < boxNumbers; y++) {
+//        boxMaker(x)
+//    }
+//}
+
+
+/*
+
+
+function totalBoxMaker() {
+    boxRemover()
+    numberSetter()
+    trueBoxerMaker()
+}
+*/
+
+/*
+
+function trueBoxerMaker() {
+    for (let x = 0; x < boxNumbers; x++)
+        lineMaker()
+        for (let y = 0; y < boxNumbers; y++) {
+            boxMaker(x)
+        }
+}
+
+function boxRemover() {
+    let child = container.firstElementChild
+    while(child) {
+        container.removeChild(child)
+        child = container.firstElementChild
+    }
+}
+
+function numberSetter() {
+    boxNumbers = document.querySelector('input').value
+    height = 100/boxNumbers
+    width = 100/boxNumbers
+}
+
+
+function totalBoxMaker() {
+    boxRemover()
+    numberSetter()
+    trueBoxerMaker()
+}
+
+*/
